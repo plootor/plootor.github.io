7ac9964502f0d7dc4ca22f991776b7f9ef92c8a5
plootor <plootor2007@gmail.com>
2014-12-08 23:07:38 +0200

    start blog post

--- allure/public/js/main.js
+++ allure/public/js/main.js
@@ -2,18 +2,10 @@
  * Created by niunea on 11/16/14.
  */
 $(function () {
-
-    var winHeight = $( window ).height();
-    $(".background div").each(function() {
-        $(this).height(winHeight);
-    });
-
-    $('.background').cycle({
-        fx:    'fade',
-        speed:  1500
-    });
-
-    $('.fluidbox').fluidbox();
-
-    $('.scroll').jScrollPane();
+    if($('.fluidbox').length){
+        $('.fluidbox').fluidbox();
+    }
+    if($('.scroll').length){
+        $('.scroll').jScrollPane();
+    }
 });
