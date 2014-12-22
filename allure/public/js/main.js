cdef622b9f693eff9221ac993ec0af5864a4a24d
Niunicica <cristina.bivol@gmail.com>
2014-12-22 14:29:00 +0200

    niorl

--- allure/public/js/main.js
+++ allure/public/js/main.js
@@ -2,9 +2,7 @@
  * Created by niunea on 11/16/14.
  */
 $(function () {
-    if($('.fluidbox').length){
-        $('.fluidbox').fluidbox();
-    }
+
     if($('.nano').length){
         $('.nano').nanoScroller();
     }
