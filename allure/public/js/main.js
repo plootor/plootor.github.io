e61328772f2cbd36cc841af4f2bc76f47fa77d02
plootor <plootor2007@gmail.com>
2015-02-17 02:12:26 +0200

    hope slider

--- allure/public/js/main.js
+++ allure/public/js/main.js
@@ -10,4 +10,22 @@ $(function () {
     if($('.nano').length){
         $('.nano').nanoScroller();
     }
+
+    $('.slider').bxSlider({
+        mode: 'fade',
+        speed: 1500,
+        infiniteLoop: true,
+        auto: true,
+        pause: 6000,
+        controls: false,
+        onSlideAfter: function(){
+            // do mind-blowing JS stuff here
+            var selectedIndex = $(".slider > :visible").index() + 1;
+            console.log('Slide index: ' + selectedIndex);
+            $('.background  > :visible').fadeOut( "slow" );
+console.log('current: ' + selectedIndex);
+            $('.background div:nth-child(' + selectedIndex + ')').fadeTo( "slow", 1 );
+        }
+
+    });
 });
