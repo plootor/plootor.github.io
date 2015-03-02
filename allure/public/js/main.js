d18aac66eb1bf859280bb6d7f0e19595307f5d73
plootor <plootor2007@gmail.com>
2015-03-03 00:52:39 +0200

    home effects

--- allure/public/js/main.js
+++ allure/public/js/main.js
@@ -17,8 +17,14 @@ $(function () {
         infiniteLoop: true,
         auto: true,
         pause: 6000,
-        controls: false,
+        controls: true,
+        autoControls: true,
         onSlideBefore: function($slideElement, oldIndex, newIndex){
+
+            //for css3 effects
+            $('.slide.active').removeClass('active');
+            $slideElement.addClass('active');
+
             var selectedIndex = newIndex + 1;
             $('.bg-home  > :visible').css('opacity', 0);
             $('.bg-home div:nth-child(' + selectedIndex + ')').css('opacity', 1);
