5efcc4131ef00000f542c306f16281c60155117c
niunea <plootor2007@gmail.com>
2015-07-18 04:59:43 +0300

    subscribe fix

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var postcss = require('gulp-postcss');
+var gulp = require('gulp');
+var autoprefixer = require('autoprefixer-core');
+var mqpacker = require('css-mqpacker');
+var csswring = require('csswring');
+
+gulp.task('css', function () {
+    var processors = [
+        autoprefixer({browsers: ['last 3 version']}),
+        mqpacker,
+        csswring
+    ];
+    return gulp.src('./flexbox2/css/style.css')
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('./dest'));
+});
