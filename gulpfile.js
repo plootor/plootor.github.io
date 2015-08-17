18485738ead10c63d874d41838c03aa8d9da4014
niunea <plootor2007@gmail.com>
2015-08-17 22:06:16 +0300

    use gulp uncss

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,22 @@
 var postcss = require('gulp-postcss');
 var gulp = require('gulp');
 var autoprefixer = require('autoprefixer-core');
+var uncss = require('gulp-uncss');
 
 gulp.task('css', function () {
     var processors = [
         autoprefixer({browsers: ['last 3 version']})
     ];
-    return gulp.src('./main/piccolo-green/css/style.css')
+    return gulp.src('./main/piccolo/css/style-processed.css')
         .pipe(postcss(processors))
+        .pipe(gulp.dest('./dest'));
+});
+
+
+gulp.task('clean', function () {
+    return gulp.src('./main/piccolo/css/style-processed.css')
+        .pipe(uncss({
+            html: ['./main/piccolo/index.html']
+        }))
         .pipe(gulp.dest('./dest'));
 });
