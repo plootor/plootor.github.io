7a9141ad3070c24bc20415a1afcbbd1618980747
niunea <plootor2007@gmail.com>
2016-01-28 00:37:06 +0200

    cleaning and js rearange

--- giu/js/main.js
+++ giu/js/main.js
@@ -1,18 +1,13 @@
 (function($) {
     "use strict";
 
-    $.fn.cycle.transitions.scrollVert = {
-        before: function( opts, curr, next, fwd ) {
-            opts.API.stackSlides( opts, curr, next, fwd );
-            var height = opts.container.css('overflow','hidden').height();
-            opts.cssBefore = { top: fwd ? height : -height, left: 0, opacity: 1, display: 'block', visibility: 'visible' };
-            opts.animIn = { top: 0 };
-            opts.animOut = { top: fwd ? -height : height };
+    $('.cycle-slideshow').on('cycle-bootstrap', function(e, opts, API) {
+        API.customGetImageSrc = function( slideOpts, opts, slideEl ) {
+            return $( slideEl ).attr('name');
         }
-    };
+    });
 
     $( ".time-show" ).click(function() {
-
         if($( ".timer-block" ).is(':visible')) {
             setTimeout(function (){
                 $( "#slides" ).fadeToggle('slow', 'swing');
@@ -28,23 +23,17 @@
             $( ".headline_small" ).fadeToggle('slow', 'swing');
             $( ".social" ).fadeToggle('slow', 'swing');
         }
-
-
-        /* $( ".hourglass").mouseleave(function(){
-         $( ".timer-block" ).hide('slow', 'swing');
-         }); */
     });
 
+    //initialize timer
+    startSecond('example-clock-container', '1', 365);
+    startSecond('example-clock-container2', '2', 24);
+    startSecond('example-clock-container3', '3', 60);
+    startSecond('example-clock-container4', '4', 60);
+
+    //initialize maps
+    google.maps.event.addDomListener(window, 'load', initialize);
 })(jQuery);
-
-// bind to C2's bootstrap event in order to get a reference to the API object
-$('.cycle-slideshow').on('cycle-bootstrap', function(e, opts, API) {
-    // add a new method to the C2 API:
-    API.customGetImageSrc = function( slideOpts, opts, slideEl ) {
-        return $( slideEl ).attr('title');
-    }
-});
-
 
 function startSecond(selector, type, fromTotal) {
 var element = document.getElementById(selector);
@@ -91,7 +80,23 @@ function daysUntil(year, month, day) {
 
     return Math.round(days);
 }
-startSecond('example-clock-container', '1', 365);
-startSecond('example-clock-container2', '2', 24);
-startSecond('example-clock-container3', '3', 60);
-startSecond('example-clock-container4', '4', 60);
+
+$.fn.cycle.transitions.scrollVert = {
+    before: function( opts, curr, next, fwd ) {
+        opts.API.stackSlides( opts, curr, next, fwd );
+        var height = opts.container.css('overflow','hidden').height();
+        opts.cssBefore = { top: fwd ? height : -height, left: 0, opacity: 1, display: 'block', visibility: 'visible' };
+        opts.animIn = { top: 0 };
+        opts.animOut = { top: fwd ? -height : height };
+    }
+};
+
+function initialize() {
+    var mapCanvas = document.getElementById('map');
+    var mapOptions = {
+        center: new google.maps.LatLng(44.5403, -78.5463),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions)
+}
