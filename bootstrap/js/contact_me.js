c4b4181aa0cb42848fbd3a1045054c25d3ebea8f
niunea <plootor2007@gmail.com>
2017-03-11 22:19:48 +0200

    test commit

--- bootstrap/js/contact_me.js
+++ bootstrap/js/contact_me.js
@@ -1,6 +1,53 @@
 // Contact Form Scripts
 
 $(function() {
+    $("#mc-embedded-subscribe-form input").jqBootstrapValidation({
+        preventSubmit: true,
+        submitError: function($form, event, errors) { console.log('inside error');
+            // additional error messages or events
+        },
+        submitSuccess: function($form, event) { console.log('inside success');
+            event.preventDefault(); // prevent default submit behaviour
+            /*
+            var email = $("input#mce-EMAIL").val();
+            $.ajax({
+                url: "././mail/contact_me.php",
+                type: "POST",
+                data: {
+                    email: email
+                },
+                cache: false,
+                success: function() {
+                    // Success message
+                    $('#subscribe-success').html("<div class='alert alert-success'>");
+                    $('#subscribe-success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#subscribe-success > .alert-success')
+                        .append("<strong>Your subscribe message has been sent. </strong>");
+                    $('#subscribe-success > .alert-success')
+                        .append('</div>');
+
+                    //clear all fields
+                    $('#mc-embedded-subscribe-form').trigger("reset");
+                },
+                error: function() {
+                    // Fail message
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
+                        .append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+                    //clear all fields
+                    $('#mc-embedded-subscribe-form').trigger("reset");
+                },
+            });
+            */
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
 
     $("#contactForm input,#contactForm textarea").jqBootstrapValidation({
         preventSubmit: true,
