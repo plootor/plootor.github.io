6ac48a04adae643cefff642a10690e475bed2a36
niunea <plootor2007@gmail.com>
2017-05-22 12:29:34 +0300

    last

--- bootstrap2/js/agency.js
+++ bootstrap2/js/agency.js
@@ -1,7 +1,7 @@
 // Agency Theme JavaScript
 
 (function($) {
-    "use strict"; // Start of use strict
+   // "use strict"; // Start of use strict
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
     $('a.page-scroll').bind('click', function(event) {
@@ -30,4 +30,37 @@
         }
     })
 
+  var toolbar = document.querySelector(".portfolio-nav");
+  toolbar.addEventListener("click", function(e) {
+    var anchor = $(e.target).attr('class');
+    if(!anchor) return false;
+    runFunc(anchor);
+    event.preventDefault();
+    event.stopPropagation();
+  });
+
 })(jQuery); // End of use strict
+
+function runFunc(type) {
+  switch (type) {
+    case 'all':
+      $('.item1').show();
+      $('.item2').show();
+      $('.item3').show();
+      $('.item4').show();
+      $('.item5').show();
+      $('.item6').show();
+      break;
+    case 'first':
+      $('.item4').hide();
+      $('.item5').hide();
+      break;
+    case 'second':
+      $('.item1').hide();
+      break;
+    case 'third':
+      $('.item3').hide();
+      break;
+  }
+  $('.grid').masonry();
+}
