14ae1bd212b56e815d0f7f2152251e5de577897d
niunea <plootor2007@gmail.com>
2017-06-08 10:52:40 +0300

    cleaning in js - scroling nice efffect mobile

--- bootstrap2/js/agency.js
+++ bootstrap2/js/agency.js
@@ -9,10 +9,14 @@
       offset: 51
   });
 
-  $('section').viewportChecker({
-    classToAdd: 'visible',
-    offset: 500,
-    repeat: false
+  // $('section').viewportChecker({
+  //   classToAdd: 'visible',
+  //   offset: 500,
+  //   repeat: false
+  // });
+  $('.fade-animate').addClass("hide2").viewportChecker({
+    classToAdd: 'vis animated fadeInUp',
+    offset: 250
   });
 
   // Closes the Responsive Menu on Menu Item Click
@@ -45,6 +49,25 @@
     runFunc(anchor);
     event.preventDefault();
     event.stopPropagation();
+  });
+
+  $(window).scroll( function(){
+
+    /* Check the location of each desired element */
+    $('.hideme').each( function(i){
+
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it it */
+      if( bottom_of_window > bottom_of_object ){
+
+        $(this).addClass('fadeInUp');
+
+      }
+
+    });
+
   });
 
 })(jQuery); // End of use strict
