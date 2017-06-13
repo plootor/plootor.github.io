a2c6bdc03cca9be0ae4e689f8f48bf59e8333dbf
niunea <plootor2007@gmail.com>
2017-06-13 23:53:02 +0300

    add some fixes

--- bootstrap2/js/agency.js
+++ bootstrap2/js/agency.js
@@ -8,17 +8,13 @@
       target: '.navbar-fixed-top',
       offset: 51
   });
-
-  // $('section').viewportChecker({
-  //   classToAdd: 'visible',
-  //   offset: 500,
-  //   repeat: false
-  // });
-  $('.fade-animate').addClass("hide-el").viewportChecker({
-    classToAdd: 'show-el animated fadeInUp',
-    offset: 80
-  });
-
+  if( !/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    // You are in mobile browser
+    $('.fade-animate').addClass("hide-el").viewportChecker({
+      classToAdd: 'show-el animated fadeInUp',
+      offset: 80
+    });
+  }
   // Closes the Responsive Menu on Menu Item Click
   $('.navbar-collapse ul li a').click(function(){
     $('.navbar-toggle:visible').click();
@@ -49,25 +45,6 @@
     runFunc(anchor);
     event.preventDefault();
     event.stopPropagation();
-  });
-
-  $(window).scroll( function(){
-
-    /* Check the location of each desired element */
-    $('.hideme').each( function(i){
-
-      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-
-      /* If the object is completely visible in the window, fade it it */
-      if( bottom_of_window > bottom_of_object ){
-
-        $(this).addClass('fadeInUp');
-
-      }
-
-    });
-
   });
 
 })(jQuery); // End of use strict
